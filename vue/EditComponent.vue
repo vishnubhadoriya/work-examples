feca9f93d23cc3f7dec6023bdc104a12297be52a
nexus_vishnu <vishnu.bhadoriya@nexuslinkservices.in>
2020-08-07 12:06:34 +0530

    Added readme for vue list component && added add,edit components

---
+++ vue/EditComponent.vue
@@ -0,0 +1,130 @@
+<template>
+  <div class="d-flex">
+    <v-card max-width="400" class="mx-auto align-self-stretch" tile :loading="loading">
+      <v-card-text class="pb-0">
+        <v-container>
+          <v-form>
+            <v-row dense>
+              <v-col cols="6">
+                <v-text-field
+                  label="Bank Name"
+                  outlined
+                  dense
+                  autofocus
+                  v-model="form_data.bank_name"
+                  :error-messages="form_error.bank_name"
+                  :hide-details="!form_error.bank_name"
+                ></v-text-field>
+              </v-col>
+              <v-col cols="6">
+                <v-text-field
+                  label="Code"
+                  outlined
+                  dense
+                  v-model="form_data.bank_code"
+                  :error-messages="form_error.bank_code"
+                  :hide-details="!form_error.bank_code"
+                ></v-text-field>
+              </v-col>
+            </v-row>
+            <v-row dense>
+              <v-col cols="6">
+                <v-text-field
+                  label="Acc. Type"
+                  outlined
+                  dense
+                  autofocus
+                  v-model="form_data.ac_type"
+                  :error-messages="form_error.ac_type"
+                  :hide-details="!form_error.ac_type"
+                ></v-text-field>
+              </v-col>
+              <v-col cols="6">
+                <v-text-field
+                  label="Acc. Group"
+                  outlined
+                  dense
+                  v-model="form_data.ac_group"
+                  :error-messages="form_error.ac_group"
+                  :hide-details="!form_error.ac_group"
+                ></v-text-field>
+              </v-col>
+            </v-row>
+          </v-form>
+        </v-container>
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn tile color="error" class="mx-5" dark @click="saveAccount" :loading="loading">Save</v-btn>
+      </v-card-actions>
+    </v-card>
+    <v-btn color="error" fixed fab bottom left to="/accounts" small>
+      <v-icon>mdi-arrow-left</v-icon>
+    </v-btn>
+  </div>
+</template>
+
+<script>
+import axios from "../../plugins/axios";
+export default {
+  name: "AccountEdit",
+  data() {
+    return {
+      loading: false,
+      form_data: {},
+      form_error: {},
+      p_type: [
+        { text: "Fixed", value: "fixed" },
+        { text: "Cost Plus", value: "cost_plus" },
+        { text: "Project Management", value: "project_management" },
+      ],
+    };
+  },
+  methods: {
+    saveAccount() {
+      this.loading = true;
+      this.form_error = {};
+      let _self = this;
+      let serverData = JSON.parse(JSON.stringify(this._data.form_data));
+      axios
+        .post("edit_account", serverData)
+        .then(function (res) {
+          let data = res.data;
+          if (!data.status) {
+            for (let key in data.errors) {
+              _self.form_error[key] = data.errors[key][0];
+            }
+          } else {
+            _self.$router.push({ path: "/accounts" });
+          }
+          _self.loading = false;
+        })
+        .catch(function (error) {
+          console.log("FAILURE!!", error);
+          _self.loading = false;
+        });
+    },
+    getBank() {
+      this.loading = true;
+      let _self = this;
+      axios
+        .get("get_account?id=" + _self.$route.params.id)
+        .then(function (res) {
+          if (res.data.status) {
+            let data = res.data.data;
+            for (let key in data) {
+              _self.form_data[key] = data[key];
+            }
+          }
+          _self.loading = false;
+        });
+    },
+  },
+  mounted: function () {
+    this.getBank();
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ vue/README.md
@@ -0,0 +1,9 @@
+# Component demo using Vue + Vuetify + Vue Router + Vuex!
+
+# Dependencies
+
+ 1. Vue (2.6.11)
+ 2. Vue Router (3.2.0)
+ 3. Vuetify (2.2.11)
+ 4. Vuex (3.5.1)
+ 5. Axios (0.19)
